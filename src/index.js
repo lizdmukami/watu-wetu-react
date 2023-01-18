cd40eb94d4399a27585332e809133abe1b1e7b46
lizdmukami <99487610+lizdmukami@users.noreply.github.com>
2023-01-18 04:11:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <RouterProvider router={root} error={errorElement} />
+        <RouterProvider router={router} error={errorElement} />
     </React.StrictMode>
 );
 
